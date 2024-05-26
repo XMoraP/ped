a35c4abdffa5192207b6d2f08b10a5d03c0f6d5c
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2024-05-26 20:15:43 +0200

    Practica 2 con fecha y hora y test

--- duli/p02/test/test.py
+++ duli/p02/test/test.py
@@ -3,11 +3,12 @@ from app.servidor import Servidor
 from datetime import datetime, date, time, timedelta
 
 class Test(unittest.TestCase):
-	
+
 	def test_fecha_hora(self):
-		resultado = obtenerFechaYhora()
+		servidor = Servidor()
+		resultado = servidor.obtenerFechaYhora()
 		fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		self.assertEquals(fecha_hora, resultado)
+		self.assertEqual(fecha_hora, resultado)
 
 if __name__ == '__main__':
 	unittest.main()

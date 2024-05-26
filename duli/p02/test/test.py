3154454bbe7e0ec0a409524bd5d900ef3451d8a8
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2024-05-26 19:45:32 +0200

    Test 1

--- duli/p02/test/test.py
+++ duli/p02/test/test.py
@@ -1,11 +1,13 @@
 import unittest
 from app.servidor import Servidor
+from datetime import datetime, date, time, timedelta
 
 class Test(unittest.TestCase):
-	def test(self):
-		servidor = Servidor()
-		ok = servidor.prueba()
-		self.assertEqual(ok, 'ok')
+	
+	def test_fecha_hora(self):
+		resultado = obtenerFechaYhora()
+		fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		self.assertEquals(fecha_hora, resultado)
 
 if __name__ == '__main__':
 	unittest.main()

a1af5de9236caed32ad3a9b5e661101b960f21b0
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2024-05-26 23:16:20 +0200

    Practica 5 con fecha y hora y test, servidor prendido

--- duli/p03/app/servidor.py
+++ duli/p03/app/servidor.py
@@ -1,20 +1,27 @@
-from datetime import datetime, date, time, timedelta
-import socket, os
+from datetime import datetime
+import socket
 
 class Servidor:
-	def obtenerFechaYhora(self):
-		resultado = datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")
-		return resultado
+    def obtenerFechaYhora(self):
+        resultado = datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")
+        return resultado
 
-	def iniciar(self):
-		servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		servidor_socket.bind(('0.0.0.0', 8888))
-		datos, address = servidor_socket.recvfrom(1024)
-		print("Cliente conectado", address)
-		mensaje = self.obtenerFechaYhora()
-		servidor_socket.sendto(mensaje.encode('utf-8'), address)
-		servidor_socket.close()
+    def iniciar(self):
+        try:
+            while True:
+                servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                servidor_socket.bind(('0.0.0.0', 8888))
+                datos, address = servidor_socket.recvfrom(1024)
+                print("Cliente conectado", address)
+                mensaje = self.obtenerFechaYhora()
+                servidor_socket.sendto(mensaje.encode('utf-8'), address)
+
+        except KeyboardInterrupt:
+            print("Deteniendo el servidor...")
+
+        finally:
+            servidor_socket.close()
 
 if __name__ == '__main__':
-	servidor = Servidor()
-	servidor.iniciar()
+    servidor = Servidor()
+    servidor.iniciar()
